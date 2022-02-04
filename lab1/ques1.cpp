984ce7b41d49027aed7067b6bb8c176a966026fc
Shohag Rana <shohagrana64@gmail.com>
2022-02-04 14:47:43 +0600

    add ques1 and 2

---
+++ lab1/ques1.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/TEST
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	while(true)
+	{
+		int n;
+		cin>>n;
+		if(n==42) break;
+	    cout << n << endl;
+	}
+	return 0;
+}
