66d44e1db851c575696abcf21e5e5be88b194f68
Shohag Rana <shohagrana64@gmail.com>
2022-02-06 23:45:33 +0600

    Update ques2.cpp

--- lab3/ques2.cpp
+++ lab3/ques2.cpp
@@ -1,1 +1,22 @@
-// 
+// https://vjudge.net/problem/CodeForces-236A
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int l,i,cnt=1;
+    set<char> s;
+    string p;
+    cin>>p;
+       for (char c: p)
+   {
+      s.insert(c);
+   }
+    if(s.size()%2==1)
+        printf("IGNORE HIM!\n");
+    else
+        printf("CHAT WITH HER!\n");
+    return 0;
+}
