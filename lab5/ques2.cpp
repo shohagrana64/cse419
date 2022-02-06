6dbb8593d03888c25bf21ed180a35a1ee1b623ad
Shohag Rana <shohagrana64@gmail.com>
2022-02-06 23:54:59 +0600

    Update ques2.cpp

--- lab5/ques2.cpp
+++ lab5/ques2.cpp
@@ -1,1 +1,36 @@
-// 
+// https://vjudge.net/problem/CodeForces-124A
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+ 
+int main()
+{
+    int n,a,b,t,count=0;
+    cin>>n>>a>>b;
+    t=n-a;
+    for(int i=a+1;i<=n;i++){
+    	if(n-i<=b){
+    		count++;
+    	}
+    }
+    cout<<count<<endl;
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// #include <string>
+// using namespace std;
+ 
+// int main()
+// {
+//     int n,p,q;
+//     cin>>n>>p>>q;
+//     if((n-p-1)>q){
+//     	cout<<q+1<<endl;
+//     }
+//     else{
+//     cout<<(n-p)<<endl;
+//     }
+//     return 0;
+// }
