b45e4ca622075f9dad596071d8f08d7327e022ba
Shohag Rana <shohagrana64@gmail.com>
2022-02-06 23:47:34 +0600

    Update ques5.cpp

--- lab3/ques5.cpp
+++ lab3/ques5.cpp
@@ -1,1 +1,22 @@
-// 
+// https://vjudge.net/problem/CodeForces-1225A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    cin >> x >> y;
+
+    if(x == y)
+        cout << x * 10 << " " << y * 10 + 1;
+    else if(x + 1 == y)
+        cout << x << " " << y;
+    else if(x == 9 && y == 1)
+        cout << "9 10";
+    else
+        cout << -1;
+
+    return 0;
+}
