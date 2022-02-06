7b2025d2057b13af784461a6c0f09e1337d7dbed
Shohag Rana <shohagrana64@gmail.com>
2022-02-06 23:46:04 +0600

    Update ques3.cpp

--- lab3/ques3.cpp
+++ lab3/ques3.cpp
@@ -1,1 +1,26 @@
-// 
+// https://vjudge.net/problem/CodeForces-263A
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+ 
+int main()
+{
+	int arr [5][5];
+	int m;
+	int p;
+	int q;
+	int sum;
+for(int i=0; i<5;i++){
+	for(int j=0;j<5;j++){
+		cin>>m;
+		if(m==1){
+			p=i;
+			q=j;
+		}
+	}
+}
+	sum= abs(p-2)+abs(q-2);
+	cout<<sum;
+	return 0;
+}
