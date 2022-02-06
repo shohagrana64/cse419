225c47b053bfacf149e3f98d00b20daaf0d37670
Shohag Rana <shohagrana64@gmail.com>
2022-02-06 23:52:21 +0600

    Update ques5.cpp

--- lab4/ques5.cpp
+++ lab4/ques5.cpp
@@ -1,1 +1,20 @@
-// 
+// https://vjudge.net/problem/LightOJ-1043
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i,test;
+    double AB,AC,BC,AD,DE,ratio;
+    cin>>test;
+    for(i=1;i<=test;i++)
+    {
+        cin>>AB>>AC>>BC>>ratio;
+        ratio = ratio/(ratio+1);
+        AD = AB*sqrt(ratio);
+        printf("Case %d: %lf\n",i,AD);
+    }
+    return 0;
+}
