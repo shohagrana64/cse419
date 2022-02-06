e0bce09e64367e1cef7971936b27e029b7595fd8
Shohag Rana <shohagrana64@gmail.com>
2022-02-06 23:50:31 +0600

    Update ques3.cpp

--- lab4/ques3.cpp
+++ lab4/ques3.cpp
@@ -1,1 +1,49 @@
-// 
+// https://vjudge.net/problem/CodeChef-SEGM01
+
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t)
+	{
+	    string s;
+	    int i,j,onePresentCheck=0,notConsecutive=0,c=0;
+	    cin>>s;
+	    for(i=0;i<s.length();i++)
+	    {
+	        if(s[i]=='1')
+	        {
+	            c++;
+	            if(c==1)
+	            {
+	                j=i;
+	                onePresentCheck=1;
+	            }
+	            else
+	            {
+	                if((i-j)==1)
+	                {
+	                    j=i;
+	                }
+	                else
+	                {
+	                    notConsecutive=1;
+	                    break;
+	                }
+	            }
+	        }
+	    }
+	    if((onePresentCheck==1)&&(notConsecutive==0))
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    cout<<"NO"<<endl;
+	    t--;
+	}
+	return 0;
+}
