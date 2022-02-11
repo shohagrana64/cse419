468817c0602e0f6612ff3ea5e1ca5215774e3225
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 00:38:26 +0600

    Update ques3.cpp

--- lab7/ques3.cpp
+++ lab7/ques3.cpp
@@ -1,1 +1,42 @@
-// 
+// https://vjudge.net/problem/UVA-11827
+
+#include <bits/stdc++.h>
+#include <cmath>
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+
+using namespace std;
+int GCD(int a,int b){
+    if(b==0){
+        return a;
+    }
+    else{
+        return GCD(b,a%b);
+    }
+}
+int main(){
+int n;
+int arr[100];
+cin>>n;
+getchar();
+string s;
+while(n--){
+    int p=0;
+    getline(cin,s);
+    stringstream ss(s);
+    while(ss>>arr[p]){
+        p++;
+    }
+    int ans=0;
+        for(int i=0;i<p;i++)
+        {
+            for(int j=i+1;j<p;j++)
+            {
+               ans=max(ans,GCD(arr[i],arr[j]));
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
