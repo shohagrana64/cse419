e82b7392717357a877298f7e3ba537e042308d11
Shohag Rana <shohagrana64@gmail.com>
2022-02-11 23:34:28 +0600

    Update ques3.cpp

--- lab6/ques3.cpp
+++ lab6/ques3.cpp
@@ -1,1 +1,14 @@
-// 
+// https://vjudge.net/problem/CodeChef-FLOW001
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,a,b;
+	cin>>t;
+	while(t--){
+		cin>>a>>b;
+		cout<<a+b<<endl;
+	}
+	return 0;
+}
