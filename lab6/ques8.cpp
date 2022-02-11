7df9821233f39d168ce1decf42c8dc5e0be0ad94
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 00:34:05 +0600

    Update ques8.cpp

--- lab6/ques8.cpp
+++ lab6/ques8.cpp
@@ -1,1 +1,23 @@
-// 
+// https://vjudge.net/problem/CodeChef-FLOW007
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,t,reversedNumber=0,remainder;
+	cin>>t;
+	while(t--){
+	reversedNumber=0;
+	remainder=0;
+    cin >> n;
+    while(n != 0) {
+    	
+        remainder = n%10;
+        reversedNumber = reversedNumber*10 + remainder;
+        n /= 10;
+    }
+
+    cout << reversedNumber<<endl;
+}
+    return 0;
+}
