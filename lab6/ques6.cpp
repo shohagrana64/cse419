ffd5df07d19e35ced782328fd17f8f1c95c12377
Shohag Rana <shohagrana64@gmail.com>
2022-02-11 23:36:11 +0600

    Update ques6.cpp

--- lab6/ques6.cpp
+++ lab6/ques6.cpp
@@ -1,1 +1,15 @@
-// 
+// https://vjudge.net/problem/HackerRank-simple-array-sum
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,p,sum=0;
+	cin>>n;
+	while(n--){
+		cin>>p;
+		sum=sum+p;
+	}
+	cout<<sum<<endl;
+	return 0;
+}
