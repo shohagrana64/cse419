41b0e38141c58f1d891cb06c4dd4668815b744f7
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 00:37:42 +0600

    Update ques2.cpp

--- lab7/ques2.cpp
+++ lab7/ques2.cpp
@@ -1,1 +1,31 @@
-// 
+// https://vjudge.net/problem/UVA-11417
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+int GCD(int a,int b){
+    if(b==0){
+        return a;
+    }
+    else{
+        return GCD(b,a%b);
+    }
+}
+int main(){
+int g=0,n;
+while(cin>>n && n!=0){
+    g=0;
+    for(int i=1;i<n;i++){
+        for(int j=i+1;j<=n;j++){
+        g+=GCD(i,j);
+        }
+    }
+    cout<<g<<endl;
+}
+return 0;
+}
+
+/*Here GCD() is a function that finds
+the greatest common divisor of the two
+input numbers*
