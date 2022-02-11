017246a9ecf58385290fdf1b49ba7f2ff951f81d
Shohag Rana <shohagrana64@gmail.com>
2022-02-11 23:33:06 +0600

    Update ques1.cpp

--- lab6/ques1.cpp
+++ lab6/ques1.cpp
@@ -1,1 +1,19 @@
-// 
+// https://vjudge.net/problem/UVA-12577
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s;
+	int c=0;
+	while(cin>>s && s!="*"){
+		c++;
+		if(s=="Hajj"){
+			cout<<"Case "<<c<<": "<<"Hajj-e-Akbar"<<endl;
+		}
+		else{
+			cout<<"Case "<<c<<": "<<"Hajj-e-Asghar"<<endl;
+		}
+	}
+	return 0;
+}
