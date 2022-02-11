2df1977d4981e46d1d18487598bd3dad6011b40e
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 00:34:43 +0600

    Update ques9.cpp

--- lab6/ques9.cpp
+++ lab6/ques9.cpp
@@ -1,1 +1,19 @@
-// 
+// https://vjudge.net/problem/CodeChef-FLOW004
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int t,n,first,last,sum;
+   cin>>t;
+   while(t--){
+    cin >> n;
+    first = n;
+	last=n % 10;
+	for(first=n;first>=10;first=first/10);
+	sum=first+last;
+	cout<<sum<<endl;
+   }
+   return 0;
+}
