6f572d024fa51d92f2fe186450fcf5094096f995
Shohag Rana <shohagrana64@gmail.com>
2022-02-11 23:33:54 +0600

    Update ques2.cpp

--- lab6/ques2.cpp
+++ lab6/ques2.cpp
@@ -1,1 +1,18 @@
-// 
+// https://vjudge.net/problem/CodeChef-FLOW017
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,a,b,c;
+	cin>>t;
+	while(t--){
+		int arr[3];
+		for(int i=0;i<3;i++){
+			cin>>arr[i];
+	}
+	sort(arr,arr+3);
+	cout<<arr[1]<<endl;
+	}
+	return 0;
+}
