e2a3e8a181a279ee31e9e08f6d38bbda38d87929
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 11:22:45 +0600

    Update ques6.cpp

--- lab9/ques6.cpp
+++ lab9/ques6.cpp
@@ -1,1 +1,26 @@
-// 
+// https://vjudge.net/problem/CodeChef-COTH001
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+int main() {
+	long long int t,a,b;
+	cin>>t;
+	while(t--){
+		cin>>a>>b;
+		if(a>b){
+            cout<<a-b<<endl;
+        }
+        else{
+            
+            int j=b%a;
+            if(j==0){
+                cout<<0<<endl;
+            }
+            else{
+                cout<<a-j<<endl;
+            }
+        }
+		}
+	return 0;
+}
