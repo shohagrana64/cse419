92c31a186b73864c8fa62098fa3948bd9bafea0c
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 11:10:58 +0600

    Update ques4.cpp

--- lab8/ques4.cpp
+++ lab8/ques4.cpp
@@ -1,1 +1,14 @@
-// 
+// https://vjudge.net/problem/AtCoder-abc179_c
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int count=0;
+    for(int i=1;i<n;i++){
+        count+=(n-1)/i;
+    }
+    cout<<count<<endl;
+    return 0;
+}
