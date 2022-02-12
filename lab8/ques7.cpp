d0fb97d224f66e09a456721b68c6672d25e7d130
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 11:12:13 +0600

    Update ques7.cpp

--- lab8/ques7.cpp
+++ lab8/ques7.cpp
@@ -1,1 +1,45 @@
-// 
+// https://vjudge.net/problem/CodeForces-472A
+
+#include <bits/stdc++.h> 
+using namespace std; 
+bool status[1000000+1]; 
+void siv(int N){
+    int sq=sqrt(N);
+    for(int i=4;i<=N;i+=2){
+        status[i]=1;
+    }
+    for(int i=3;i<=sq;i+=2){
+        if(status[i]==0){
+            for(int j=i*i;j<=N;j+=i)
+                status[j]=1;
+        }
+    }
+    status[1]=1;
+}
+int main() 
+{
+    int n;
+    siv(1000000);
+    // for(int i=0;i<10;i++){
+    //     cout<<status[i]<<endl;
+    // }
+    while(cin>>n && n!=0){
+        int startp=3, endp=n-1, count=0;
+        while(startp<=endp){
+            if(status[startp]+status[endp]==2 && startp+endp==n){
+                cout<<startp<<" "<<endp<<endl;
+                break;
+            }
+            else if(startp+endp<n){
+                startp++;
+                //cout<<startp<<" ";
+            }
+            else{
+                endp--;
+                //cout<<endp<<" ";
+            }
+        }
+        
+    }
+    return 0; 
+}
