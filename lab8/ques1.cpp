98b9f1e95b0d98a791f802668e83f4c80e2eccbb
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 11:08:45 +0600

    Update ques1.cpp

--- lab8/ques1.cpp
+++ lab8/ques1.cpp
@@ -1,1 +1,29 @@
-// 
+// https://vjudge.net/problem/SPOJ-NOTATRI
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    while(cin>>n && n!=0){
+   	    int a[n],i,j,k,count=0;
+   	    for(i=0;i<n;i++){
+   		    cin>>a[i];
+   	    }
+   	sort(a,a+n);
+   	for(i=n-1;i>=0;i--){
+   		j=0;
+   		k=i-1;
+   		while(k>j){
+   			if(a[j]+a[k]<a[i]){
+   				count+=k-j;
+   				j++;
+   			}
+   			else{
+   			    k--;
+               }
+   		}			
+   		}
+   		cout<<count<<endl;
+   	}
+    return 0;
+}
