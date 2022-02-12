1ff17893ffe3332d776a066530701cb34dc50be5
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 11:20:37 +0600

    update lab9

--- lab9/ques3.cpp
+++ lab9/ques3.cpp
@@ -1,1 +1,16 @@
-// 
+// https://codeforces.com/problemset/problem/959/A
+
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+int main() {
+    int a;
+	cin>>a;
+    if(a%2==0){
+        cout<<"Mahmoud"<<endl;
+    }
+	else{
+        cout<<"Ehab"<<endl;
+    }
+	return 0;
+}
