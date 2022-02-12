0d5b657e6d98cfdbe7496b66c6d1a6d32711dd7b
Shohag Rana <shohagrana64@gmail.com>
2022-02-12 11:12:50 +0600

    Update ques8.cpp

--- lab8/ques8.cpp
+++ lab8/ques8.cpp
@@ -1,1 +1,17 @@
-// 
+// https://vjudge.net/problem/CodeForces-822A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,i,ans=1;
+    cin>>a>>b;
+    for(i=1;i<=min(a,b);i++)
+    {
+            ans*=i;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
